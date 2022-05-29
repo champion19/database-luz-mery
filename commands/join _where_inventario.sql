92a51db93651fc94f7cba5740ced4055f1411725
Emmanuel Londoño :) <emmalondogo@gmail.com>
2022-05-29 10:21:45 -0500

    add join_where_inventario.sql

--- commands/join _where_inventario.sql
+++ commands/join _where_inventario.sql
@@ -1,4 +1,4 @@
-SELECT * FROM inventario
+SELECT ciudad.nombre FROM inventario
 INNER JOIN farmacia ON inventario.farmacia_id = farmacia.id
 INNER JOIN ciudad ON farmacia.ciudad_id = ciudad.id
 WHERE cantidad BETWEEN 2 AND  5 AND ciudad.nombre = "Rionegro";
