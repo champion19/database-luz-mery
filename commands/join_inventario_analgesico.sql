e300aaee292e9bdce7d7ab7077994f94aa8e600b
Emmanuel Londoño :) <emmalondogo@gmail.com>
2022-05-29 11:49:17 -0500

    add join_inventario_analgesico.sql

--- commands/join_inventario_analgesico.sql
+++ commands/join_inventario_analgesico.sql
@@ -1,6 +1,9 @@
-SELECT * FROM inventario i 
+SELECT f.calle,m.nombre, t.nombre,d.descripcion,concat(c.nombre,",",d.descripcion) AS CIUDAD
+FROM inventario i 
 INNER JOIN farmacia f ON f.id=i.farmacia_id
+INNER JOIN ciudad c ON f.ciudad_id =c.id 
+INNER JOIN departamentof d ON d.codigo =c.coddepto
 INNER JOIN medicamento m ON i.medicamento_id =m.id 
-INNER JOIN medicamentoterapia m2 ON  m2.medicamento_id =m.id 
-INNER jOIN terapeuticas t ON m2.terapeuticas_id = t.id
+INNER JOIN medicamentoterapia m2 ON m2.medicamento_id =m.id 
+INNER JOIN terapeuticas t ON m2.terapeuticas_id = t.id
 WHERE t.nombre LIKE "%analgesico%";
